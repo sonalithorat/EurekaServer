9b3da1a0d8af4aa06dc7c8ef4506ab325fd0a75a
sonalithorat <sonalithorat17@gmail.com>
2023-10-23 18:39:47 +0530

    initial commit

---
+++ src/test/java/com/sonali/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonali.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
